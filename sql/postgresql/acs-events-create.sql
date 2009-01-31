d456eb5230204a2b553301cc01dee478c5910642
cvs2svn <oct@openacs.org>
2009-01-31 13:49:51 +0000

    This commit was manufactured by cvs2svn to create branch 'b3-5-0-patches'.
    Sprout from master 2005-12-03 18:23:29 UTC Klaus Hofeditz  <klaus.hofeditz@project-open.com> 'Initial import'
    Cherrypick from b3-4-0-patches 2009-01-31 13:49:39 UTC cvs2svn <oct@openacs.org> 'This commit was manufactured by cvs2svn to create branch 'b3-4-0-patches'.':
        sql/postgresql/oracle-compat-create.sql
        acs-events.info
        sql/postgresql/acs-events-create.sql

--- sql/postgresql/acs-events-create.sql
+++ sql/postgresql/acs-events-create.sql
@@ -669,14 +669,14 @@ create function acs_event__get_html_p (
 )
 returns boolean as '	-- acs_events.html_p%TYPE
 declare
-       get_html_p__event_id    in acs_events.event_id%TYPE 
+       get_html_p__event_id    acs_events.event_id%TYPE;
        v_html_p		acs_events.html_p%TYPE; 
 begin
        select coalesce(e.html_p, a.html_p) into v_html_p
        from  acs_events e
        left join acs_activities a
        on (e.activity_id = a.activity_id)
-       where e.event_id = get_html_p__event_id
+       where e.event_id = get_html_p__event_id;
 
        return v_html_p;
 
@@ -697,14 +697,14 @@ create function acs_event__get_status_summary (
 )
 returns boolean as '	-- acs_events.status_summary%TYPE
 declare
-       get_status_summary__event_id    in acs_events.event_id%TYPE 
+       get_status_summary__event_id    acs_events.event_id%TYPE;
        v_status_summary		acs_events.status_summary%TYPE; 
 begin
        select coalesce(e.status_summary, a.status_summary) into v_status_summary
        from  acs_events e
        left join acs_activities a
        on (e.activity_id = a.activity_id)
-       where e.event_id = get_status_summary__event_id
+       where e.event_id = get_status_summary__event_id;
 
        return v_status_summary;
 
